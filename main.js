f97996e530812e4b151233b7f0db47a28d14ce61
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-21 11:27:07 -0300

    updates

---
+++ main.js
@@ -0,0 +1,20 @@
+import data from './json/livroDeMormon.json' assert { type: 'json' };
+
+const cardContainer = document.querySelector("[card-container]");
+const cardTemplate = document.querySelector("[card-template]");
+
+// Render all cards
+function renderCards(data, container) {
+	data.forEach(escritura => {
+		const card = cardTemplate.content.cloneNode(true).children[0];
+		console.log(card);
+		const referencia = card.querySelector("[card-referencia]");
+		const palavras = card.querySelector("[card-palavras]");
+		referencia.textContent = escritura.referencia;
+		palavras.textContent = escritura.palavras;
+		container.append(card);
+	});
+}
+
+renderCards(data, cardContainer);
+
