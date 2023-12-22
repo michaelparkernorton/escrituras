ed9f28a88ff64697fb6048b139dbcd8f06c86b12
michaelparkernorton <michaelparkernorton.va@gmail.com>
2023-12-22 09:02:53 -0300

    removed extras

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const cardTemplate = document.querySelector("[card-template]");
 // Render all cards
 function renderCards(data, container) {
 	data.forEach(escritura => {
-		const card = cardTemplate.content.cloneNode(true).children[0];
-		const referencia = card.querySelector("[card-referencia]");
-		const palavras = card.querySelector("[card-palavras]");
-		referencia.textContent = escritura.referência;
-		palavras.textContent = escritura.palavras;
-		container.append(card);
+
+		if (escritura.referência !== "") {
+			const card = cardTemplate.content.cloneNode(true).children[0];
+			const referencia = card.querySelector("[card-referencia]");
+			const palavras = card.querySelector("[card-palavras]");
+			referencia.textContent = escritura.referência;
+			palavras.textContent = escritura.palavras;
+			container.append(card);
+		}
 	});
 }
 
